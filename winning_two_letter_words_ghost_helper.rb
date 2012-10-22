05d10440fc348ab188d70bb1787332d35b525e1b
Rory O’Kane <rory@roryokane.com>
2012-10-21 22:52:12 -0400

    Initial commit

---
+++ winning_two_letter_words_ghost_helper.rb
@@ -0,0 +1,18 @@
+# load wordlist
+wordlist = Array.new
+File.open("enable1.txt", "r") do |wordlist_file|
+	wordlist_file.each_line do |line|
+		wordlist.push(line.strip)
+	end
+end
+
+# calculate guaranteed-win n-letter words, assuming certain rules about word length
+# those rules are “n-letter and shorter words are safe” and
+# “n-letter and shorter words are allowable responses to ‘I challenge’”
+target_word_length = 2
+two_letter_words = wordlist.reject {|word| word.length != target_word_length}
+puts "#{target_word_length}-letter words that start no other words:"
+two_letter_words.each do |two_letter_word|
+	words_starting_with_the_tlw = wordlist.reject { |word| ! word.start_with?(two_letter_word) || word.length < (target_word_length + 1) } [0..10]
+	puts two_letter_word if words_starting_with_the_tlw.empty?
+end
