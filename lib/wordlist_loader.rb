8c0b4ca12f2acb4dfed4eea9cc69826556fff39d
Rory O’Kane <rory@roryokane.com>
2012-12-03 13:47:14 -0500

    shorten load_wordlist

--- lib/wordlist_loader.rb
+++ lib/wordlist_loader.rb
@@ -1,12 +1,8 @@
 # encoding: utf-8
 
 def load_wordlist
-	wordlist = Array.new
 	wordlist_file_path = "lib/enable1.txt"
-	File.open(wordlist_file_path, 'r') do |wordlist_file|
-		wordlist_file.each_line do |line|
-			wordlist.push(line.strip)
-		end
+	return File.open(wordlist_file_path, 'r') do |wordlist_file|
+		wordlist_file.each_line.map(&:strip)
 	end
-	return wordlist
 end
