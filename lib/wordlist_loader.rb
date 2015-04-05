76028b40be018d7453750f7031f8828f520afcf3
Rory O’Kane <rory@roryokane.com>
2015-04-04 23:55:34 -0400

    Move enable1.txt from lib/ to config/
    It seems like a more applicable folder. Separate code from plain-text configuration data.

--- lib/wordlist_loader.rb
+++ lib/wordlist_loader.rb
@@ -1,5 +1,5 @@
 def load_wordlist
-	wordlist_file_path = "lib/enable1.txt"
+	wordlist_file_path = "config/enable1.txt"
 	return File.open(wordlist_file_path, 'r') do |wordlist_file|
 		wordlist_file.each_line.map(&:strip)
 	end
