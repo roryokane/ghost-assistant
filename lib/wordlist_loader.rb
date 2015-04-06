0db87263bfcfa1b9db4ca1650e2c2fe41f150e58
Rory O’Kane <rory@roryokane.com>
2015-04-05 23:17:42 -0400

    Don’t bother stripping wordlist, and refactor
    The refactoring: making `wordlist_file_path` a constant. It makes the method easier to read.
    
    The wordlist is already stripped. I checked. I doubt spaces will creep in even after I update the wordlist. And stripping probably makes the loading slower.

--- lib/wordlist_loader.rb
+++ lib/wordlist_loader.rb
@@ -1,6 +1,7 @@
+WORDLIST_FILE_PATH = "config/enable1.txt"
+
 def load_wordlist
-	wordlist_file_path = "config/enable1.txt"
-	return File.open(wordlist_file_path, 'r') do |wordlist_file|
-		wordlist_file.each_line.map(&:strip)
+	return File.open(WORDLIST_FILE_PATH, 'r') do |wordlist_file|
+		wordlist_file.each_line.to_a
 	end
 end
