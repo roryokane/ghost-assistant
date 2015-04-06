33d576dcc99650d19c49b25a271641a8bc102360
Rory O’Kane <rory@roryokane.com>
2015-04-06 16:17:47 -0400

    Fix bug where words in list included newlines
    The `strip` wasn’t necessary for arbitrary whitespace, but it was necessary to remove the newline. I hadn’t noticed that.

--- lib/wordlist_loader.rb
+++ lib/wordlist_loader.rb
@@ -2,6 +2,6 @@ WORDLIST_FILE_PATH = "config/enable1.txt"
 
 def load_wordlist
 	return File.open(WORDLIST_FILE_PATH, 'r') do |wordlist_file|
-		wordlist_file.each_line.to_a
+		wordlist_file.each_line.map(&:chomp)
 	end
 end
