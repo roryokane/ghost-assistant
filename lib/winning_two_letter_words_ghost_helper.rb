a12c85a6f09f034f2c815bbe337f1d79e1e9ea78
Rory O’Kane <rory@roryokane.com>
2015-04-05 21:08:19 -0400

    Refactor winning_to_letter_words_ghost_helper
    I must not have known of the `select` method when I first wrote this.

--- lib/winning_two_letter_words_ghost_helper.rb
+++ lib/winning_two_letter_words_ghost_helper.rb
@@ -1,14 +1,17 @@
-require_relative 'wordlist_loader'
-
-wordlist = load_wordlist()
-
 # calculate guaranteed-win n-letter words, assuming certain rules about word length
 # those rules are “n-letter and shorter words are safe” and
 # “n-letter and shorter words are allowable responses to ‘I challenge’”
+
+require_relative 'wordlist_loader'
+
+wordlist = load_wordlist()
 target_word_length = 2
-two_letter_words = wordlist.reject {|word| word.length != target_word_length}
+two_letter_words = wordlist.select {|word| word.length == target_word_length}
+
 puts "#{target_word_length}-letter words that start no other words:"
 two_letter_words.each do |two_letter_word|
-	words_starting_with_the_tlw = wordlist.reject { |word| ! word.start_with?(two_letter_word) || word.length < (target_word_length + 1) } [0..10]
-	puts two_letter_word if words_starting_with_the_tlw.empty?
+	starts_no_other_words = wordlist.none? do |word|
+		word.start_with?(two_letter_word) && word.length >= (target_word_length + 1)
+	end
+	puts two_letter_word if starts_no_other_words
 end
